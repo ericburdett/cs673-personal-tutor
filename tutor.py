6f243bf0fccda0ace2129f1feabbd7354c1c0877
Stephen Cowley <stephencowley44@gmail.com>
2020-04-17 17:31:08 -0600

    Minor fixes and add starting words

--- tutor.py
+++ tutor.py
@@ -1,12 +1,18 @@
+import time
 from language import WordDist, LanguageModel, Evaluator
 from user import UserKnowledge
 from system import ReaderSystem
 import numpy as np
 import spacy
 
-K = 15
+K = 50
 MAX_SENTENCE_LENGTH = 60
 NUM_SENTENCES = 25
+KNOWN = 0
+print(f'K: {K}')
+print(f'MAX_SENTENCE_LENGTH: {MAX_SENTENCE_LENGTH}')
+print(f'NUM_SENTENCES: {NUM_SENTENCES}')
+print(f'KNOWN: {KNOWN}')
 
 NEWS_POLITICS_PROMPTS = ['President Trump said',
                          'CNN News Report:']
@@ -34,11 +40,14 @@ def main():
   print("[0]: News/Politics")
   print("[1]: Sports")
   print("[2]: Coronavirus")
-  
+
   topic = -1
-  while topic not in ["0", "1", "2"]:
-    topic = input("Topic Number: ")
-  
+  while topic not in [0, 1, 2]:
+    try:
+        topic = int(input("Topic Number: "))
+    except ValueError:
+        print('Topic should be a number')
+
   print('\nLoading Language Learning System...')
 
   reader_system = ReaderSystem()
@@ -48,7 +57,17 @@ def main():
   lm = LanguageModel(k=K)
   lm.set_mask(np.ones(len(lm.tokenizer.get_vocab())))
 
-  user_knowledge = UserKnowledge(WordDist().dict_normalized(), lm.tokenizer)
+  word_dist = WordDist()
+
+
+  user_knowledge = UserKnowledge(word_dist.dict_normalized(), lm.tokenizer)
+
+  sorted_words = sorted(
+    ((k, v) for k, v in word_dist.dict_normalized().items()),
+    key=lambda kv: kv[1], reverse=True)
+
+  for i in range(KNOWN):
+    user_knowledge.update([sorted_words[i][0]]*5, [True]*5)
 
   if topic == 0: # News/Politics
     prompts = NEWS_POLITICS_PROMPTS
@@ -58,10 +77,13 @@ def main():
     prompts = SPORTS_PROMPTS
     prompts_truncate = SPORTS_TRUNCATE
     topic = "Sports"
-  else:
+  elif topic == 2:
     prompts = CORONAVIRUS_PROMPTS
     prompts_truncate = CORONAVIRUS_TRUNCATE
     topic = "Health"
+  else:
+    raise ValueError('bad topic somehow')
+
 
   evaluator = Evaluator(topic, nlp)
 
@@ -73,11 +95,15 @@ def main():
     prompt = prompts[rand_index]
     should_truncate = prompts_truncate[rand_index]
 
+    # Get/set the mask
+    new_mask = user_knowledge.compute_mask()
+    lm.set_mask(new_mask)
+
     # Get sentences from the language model
     sentences = lm.get_sentences(prompt, MAX_SENTENCE_LENGTH, NUM_SENTENCES)
     if should_truncate:
       sentences = [remove_prompt(sentence, prompt) for sentence in sentences]
-    
+
     # Score the sentences based on evaluation criteria
     scores = evaluator.score_sentences(sentences)
     best_sentence = sentences[np.argmax(scores)]
@@ -87,9 +113,20 @@ def main():
 
     # Update our user knowledge and recompute the language model mask
     user_knowledge.update(words, word_knowledge)
-    new_mask = user_knowledge.compute_mask()
-    lm.set_mask(new_mask)
-    
 
 if __name__ == "__main__":
   main()
+
+  # lm = LanguageModel(k=K)
+  # lm.set_mask(np.ones(len(lm.tokenizer.get_vocab())))
+
+  # word_dist = WordDist()
+
+
+  # user_knowledge = UserKnowledge(word_dist.dict_normalized(), lm.tokenizer)
+
+  # sorted_words = sorted(
+  #   ((k, v) for k, v in word_dist.dict_normalized().items()),
+  #   key=lambda kv: kv[1], reverse=True)
+  # for i in range(KNOWN):
+  #   user_knowledge.update([sorted_words[i][0]]*5, [True]*5)
