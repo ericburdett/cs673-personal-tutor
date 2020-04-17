efbb09a82aba538526eab0aa06ffc4861866b1ae
Stephen Cowley <stephencowley44@gmail.com>
2020-04-17 17:31:21 -0600

    Spacing

--- user.py
+++ user.py
@@ -30,7 +30,8 @@ class WordMemory:
 
 
 class UserKnowledge:
-  def __init__(self, vocabulary_list, tokenizer, window=500, lower_bound=.9, upper_bound=1.):
+  def __init__(self, vocabulary_list, tokenizer, window=500,
+               lower_bound=.10, upper_bound=1.):
     # contains dictionary of word : WordMemoryObject
     self.ranking = dict(enumerate(vocabulary_list))
     self.knowledge = {word: WordMemory(rank=index)
